513ba71f7a9b844e2e2b638832309e6ab0e91555
cadentj <caden.juang@gmail.com>
2026-02-02 22:59:12 -0500

    cleanup

--- caden/image.py
+++
@@ -1,6 +0,0 @@
-import modal
-
-image = (
-    modal.Image.debian_slim()
-    .uv_pip_install("pandas==2.2.0", "numpy")
-)
--- caden/main.py
+++
@@ -1,8 +0,0 @@
-# %%
-
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-0.6B")
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6B")
-
-# %%
